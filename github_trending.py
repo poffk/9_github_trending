89364e4a29fba103f7c28219107490761f9e32b1
Alexander <Alexander>
2016-12-10 14:59:15 +0300

    Рабочая версия кода, первая отправка.

--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,34 @@
+import requests
+import json
+from datetime import date, timedelta
+
+
+TOP_SIZE = 20
 
 
 def get_trending_repositories(top_size):
-    pass
+    week_ago_date = date.today() - timedelta(days=7)
+    sorted_repositories = requests.get('https://api.github.com/search/repositories?' \
+                                              'q=created:>{}&sort=stars'.format(week_ago_date))
+    return sorted_repositories.json()['items'][:top_size]
 
 
-def get_open_issues_amount(repo_owner, repo_name):
-    pass
+def get_open_issues_links(repo_owner, repo_name):
+    all_issues = requests.get('http://api.github.com/repos/{0}/{1}/issues' .format(repo_owner, repo_name))
+    issues_links = [issue['html_url'] for issue in all_issues.json()]
+    return issues_links
+
+
+def print_beauty(owner_name, repo_name, stars, issues_quantity, issues_links):
+    print('{owner}\'s repository {repo_name} has {stars_q} stars and {iss_q} open issues.\nLinks to issues:' \
+          .format(owner=owner_name, repo_name=repo_name, stars_q=stars, iss_q=issues_quantity))
+    for link in issues_links:
+        print(link)
+    print()
 
 if __name__ == '__main__':
-    pass
+    trending_repo_sorted = get_trending_repositories(TOP_SIZE)
+    for item in trending_repo_sorted:
+        issues_links = get_open_issues_links(item['owner']['login'], item['name'])
+        print_beauty(item['owner']['login'], item['name'], \
+                    item['stargazers_count'], len(issues_links), issues_links)
